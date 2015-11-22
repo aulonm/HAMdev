dc89bbdf187cf72385ae6d33095e92837033337a
magnurh <magnus.hestvik@gmail.com>
2015-11-22 16:42:49 +0100

    Added dependencies to angular google maps. Attempting to import and show map

--- main.js
+++ main.js
@@ -8,13 +8,14 @@
     /* Lager main moduler, og saa sier vi at de mindre modulene skal ta ting fra disse,
     eller bygges videre paa disse*/
     angular.module('mainServices', ['ngResource']);
-    angular.module('mainControllers', ['mainServices']);
+    var app = angular.module('mainControllers', ['mainServices', 'uiGmapgoogle-maps']);
 
     /* Define modules */
-    var app  = angular.module('main', ['ngRoute', 'mainControllers']);
 
-
-
+    /* controller for google maps */
+    angular.controller('GoogleMapCtr', function($scope){
+        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    });
     /* Her redirecter den til den sida som vi vil skal vises, fyller opp index.html fila */
     app.config(['$routeProvider',
         function($routeProvider){
