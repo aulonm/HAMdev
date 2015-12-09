999eb4a746d1cb90f9b1d1a86670b868037d95a4
Aulon Mujaj <aulon.mujaj@gmail.com>
2015-12-09 02:21:53 +0100

    added a showPolygons when starting the site, and specified that it will show all the facilities from Sierra Leone first, just so I could test if I'm getting the correct data. Loads SUPER SLOW when plotting every single coordinate to the map.

--- js/mapsController.js
+++ js/mapsController.js
@@ -7,14 +7,52 @@
             init();
             function init(){
                 var setMainMap = function(){
-                    $scope.map = {center: {latitude: 8.536426, longitude: -11.896692}, zoom: 7};
+                    $scope.map = {center: {latitude: 8.536426, longitude: -11.896692}, zoom: 8};
                 };
                 apiService.getFacilitiesOnLevel(2).get(function(result){
                     $scope.facilities = result.organisationUnits;
                     $scope.levelParent = $scope.facilities[0].parent;
                     console.log($scope.facilities[0]);
+                    polygonsOnMap();
                     setMainMap();
                 })
+            }
+
+            function polygonsOnMap(){
+                $scope.unitPolygons = [];
+
+                function pushPolygonsToMap(id, facility){
+                    var coordinates = [];
+
+                    if(!facility.coordinates){
+                        return;
+                    }
+
+                    var geoData = JSON.parse(facility.coordinates)[0][0];
+
+                    for (var i = 0; i < geoData.length; i++){
+                        var x = geoData[i][0];
+                        var y = geoData[i][1];
+                        coordinates.push({latitude: y, longitude: x});
+                    }
+
+                    $scope.unitPolygons.push({
+                        id: id,
+                        path: coordinates,
+                        stroke: {
+                            color: '#6060FB',
+                            weight: 3
+                        },
+                        editable: true,
+                        draggable: false,
+                        geodesic: false,
+                        visible: true
+                    });
+                }
+                for (var id in $scope.facilities){
+                    var facility = $scope.facilities[id];
+                    pushPolygonsToMap(id, facility);
+                }
             }
 
 
@@ -37,7 +75,7 @@
 
             // uiGmapGoogleMapApi is a promise.
             // The "then" callback function provides the google.maps object.
-            uiGmapGoogleMapApi.then(function (maps) {
+            /*uiGmapGoogleMapApi.then(function (maps) {
                 $scope.map = {
                     center: {latitude: 8.536426, longitude:  -11.896692},
                     zoom: 8,
@@ -52,11 +90,11 @@
                                     $scope.map.center = center;
                                 });
                             } else {
-                                /* We are not allowed to track location */
+                                /!* We are not allowed to track location *!/
                             }
                         }
                     },
-                    /* Dummy data for showing Markers and Polygons */
+                    /!* Dummy data for showing Markers and Polygons *!/
                     markers: [
                         {
                             id: 1,
@@ -116,7 +154,7 @@
                         }
                     ]
                 };
-            });
+            });*/
         }]);
 
 
